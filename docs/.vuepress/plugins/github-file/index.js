2da4ab8532de07788732de96f748bff8bacecb1d
langnang <langnang.chen@outlook.com>
2022-06-22 17:10:26 +0800

    plugin-github-file

--- docs/.vuepress/plugins/github-file/index.js
+++ docs/.vuepress/plugins/github-file/index.js
@@ -20,13 +20,28 @@ const plugin = (options = {}, ctx) => {
         const files = page._content.split('\r\n').filter(line => /^<<< @github-file/.test(line))
         await axios.all(files.map(path => {
           const [user, repo, blob, branch, ...file] = path.substr('<<< @github-file/'.length).split('/');
-          const filePath = file.join('/')
+          let filePath = file.join('/')
+          let snippets = [];
+          if (filePath.indexOf(`{`) !== -1) {
+            snippets = filePath.slice(filePath.indexOf(`{`) + 1, -1).split(',')
+            console.log(snippets)
+            filePath = filePath.slice(0, filePath.indexOf(`{`))
+          }
           const url = `https://api.github.com/repos/${user}/${repo}/contents/${filePath}?ref=${branch}`;
           console.log(`[GET GitHub File] ${url}`)
-          return axios.get(encodeURI(url)).then(res => Promise.resolve({ line: path, ...res.data }))
+          return axios.get(encodeURI(url)).then(res => Promise.resolve({ line: path, snippets, ...res.data }))
         })).then(res => {
           res.forEach(item => {
-            page._content = page._content.replace(item.line, `${Buffer.from(item.content, item.encoding).toString('utf8')}\r\n`)
+            let _content = Buffer.from(item.content, item.encoding).toString('utf8')
+            if (item.snippets.length !== 0) {
+              // 提取代码片段
+              const content = _content.split('\n');
+              _content = item.snippets.map(inteval => {
+                inteval = inteval.split('-').map(v => Number(v))
+                return content.slice(inteval[0], inteval[1] || inteval[0] + 1).join('\n');
+              }).join('\n...\n')
+            }
+            page._content = page._content.replace(item.line, `${_content}\r\n`)
           });
         })
         return {
